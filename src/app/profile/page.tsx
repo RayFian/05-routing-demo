3016e9b9cd9d544821dc0bb1ebb3de9e0d947515
RayFian <ray.fian7886@gmail.com>
2024-03-15 15:07:35 +0700

    WS 05: Jawaban Praktikum 01

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <h1>Welcome to About</h1>
+            <h2>Name: Raynor Herfian Iqbal Fawwaz</h2>
+            <h2>Class: TI - 3I</h2>
+            <h2>NIM: 2141720260</h2>
+        </div>
+    );
+}
